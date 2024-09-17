f794dc4b3530215c9ad04a1f4ebe616fd08f7ccc
dragen md <nimsaravihaga569@gmail.com>
2024-09-17 10:05:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "Session~GjSWmbL4",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/38a4ba6f7a51c15f5feaf.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "Hello i am Test-MD i am alive now ❤️‍🩹@&",    
 };
